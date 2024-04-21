3ff059f752b66b5e7a01351518d8faff5a0222b5
17naved <navedpatel2000@gmail.com>
2024-04-21 18:52:38 +0530

    Fullproject

---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const scaleFactor = 4;
+export const dialogueData = {
+    pc: 'This is my PC, I work here on my projects, if you want check it out you can do on my <a href="https://github.com/PatellAli" target="_blank">GitProfile.</a>',
+    tv: 'This is my tv, I watch here Youtube videos, I watch tutorials on Website Development/Designing.I can recommand you a youtube channel to learn <a href="https://www.youtube.com/@freecodecamp" target="_blank">FreeCodeCamp</a>, do check it out...',
+   "sofa-table": 'This is my sofa',
+   "cs-degree":'This is my Education, I am studying Diploma IT Engineering from Shri Bhagubhai Mafatlal Polytecnique. I passed my 10th standard from St. Blaise High School in 2022-23. ',
+   exit:'If you want to exit close this tab.',
+   "resu projects":'this is my Resume------><a href="https://github.com/PatellAli/Resume/blob/main/ali_resume.pdf" target="_blank">Resume</a>',
+   library:'Want to read ???',
+   bed:'Want to Sleep already??'
+};
